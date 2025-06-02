38335779e648db0e1902728e8baee419b4c175c5
BBee98 <blue98.bee.dev@gmail.com>
2025-06-03 00:12:37 +0200

    - Add customizing options for text-block

--- src/components/CustomizationBar/index.tsx
+++ src/components/CustomizationBar/index.tsx
@@ -1,20 +1,27 @@
 import type {ReactNode} from "react";
 import type {Key} from "@domain/types.ts";
 import {ComponentCustomizationOptions} from "@domain/Component.ts";
-import type {CustomizationOptionsProps} from "@domain/Customization/types.ts";
+import type {CustomizationOptionsProps, CustomizationProps} from "@domain/Customization/types.ts";
 import {CustomizationList} from "@domain/Customization/List.ts";
 
 import styles from "./styles.module.css";
 
-const OptionTab = ({ options }: { options: Array<keyof CustomizationOptionsProps> }) => {
+const OptionTag = ({icon}: CustomizationProps) => {
+    const IconTag = icon;
+    return <li className={styles.iconWrapper}>{
+        IconTag && <IconTag className={styles.icon}/>
+    }</li>
+}
+
+
+const OptionBar = ({ options }: { options: Array<keyof CustomizationOptionsProps> }) => {
     return (
-        <>
-            { options?.map( (key) => {
+        <ul className={styles.optionBar}>
+            {options?.map((key, index) => {
                 const props = CustomizationList[key];
-                const IconTag = props.icon;
-                return IconTag && <IconTag key={key} />
+                return <OptionTag key={`${key}-${index}`} {...props}/>
             })}
-        </>
+        </ul>
     )
 }
 
@@ -22,11 +29,11 @@ export const CustomizationBar = ({id, children}: {
     id: Key,
     children: ReactNode}) => {
 
-    return <ul>
-        { ComponentCustomizationOptions[id].map( (option) => {
-            const availableOptions: Array<keyof CustomizationOptionsProps> = Object.keys(option).filter( k => option[k as keyof CustomizationOptionsProps] )
-            return availableOptions.length > 0 && <OptionTab options={availableOptions} />
-        })}
+    return <div className={styles.wrapper}>
         <div tabIndex={0} className={styles.customizeBar}>{children}</div>
-    </ul>
+        { ComponentCustomizationOptions[id].map( (option , index) => {
+            const availableOptions: Array<keyof CustomizationOptionsProps> = Object.keys(option).filter( k => option[k as keyof CustomizationOptionsProps] )
+            return availableOptions.length > 0 && <OptionBar key={`opt-${index}`} options={availableOptions} />
+        })}
+    </div>
 }
